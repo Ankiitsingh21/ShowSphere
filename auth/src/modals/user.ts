f035952f87528b7240b1bd65634078f96858bf25
Ankit Singh <ankiitsingh21@gmail.com>
2026-03-01 08:54:37 +0530

    added user modal with strictly typed with typescript

---
+++ auth/src/modals/user.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+
+//An interface that describes the properties 
+//that are required to create a new user
+interface UserAttrs {
+        email:string;
+        password:string;
+}
+
+//An interface that describes the properties
+//that a user model has
+interface UserModal extends mongoose.Model<any>{
+        build(attrs:UserAttrs):any
+}
+
+const userSchema = new  mongoose.Schema({
+        email:{
+                type:String,
+                required:true
+        },
+        password:{
+                type:String,
+                required:true
+        }
+});
+
+userSchema.statics.build=(attrs:UserAttrs)=>{
+         return new User(attrs);
+}
+
+const User = mongoose.model<any,UserModal>('User',userSchema);
+
+
+
+
+export {User};
