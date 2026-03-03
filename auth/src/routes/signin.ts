709cca17baccab546c1366b93afb3ba9501cdf73
Ankit Singh <ankiitsingh21@gmail.com>
2026-03-03 09:11:34 +0530

    completed user route with proper error handling

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,51 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { User } from "../modals/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("hi There !!!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("You must send a password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password,
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("INvalid Credentials");
+    }
+
+    const exisitingUserjwt = await jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    req.session = {
+      jwt: exisitingUserjwt,
+    };
+
+    return res.status(200).send(existingUser);
+  },
+);
 
 export { router as signinRouter };
