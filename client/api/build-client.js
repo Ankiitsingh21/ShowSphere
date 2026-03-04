c9f1f3bed677093a95e3b6f93c0db2b6a2c0d842
Ankit Singh <ankiitsingh21@gmail.com>
2026-03-04 20:42:27 +0530

    pretefied the code

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
-export default ({ req }) => {
+const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
-    // We are on the server
-
+    // Server side
     return axios.create({
       baseURL:
         "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
-    // We must be on the browser
+    // Client side
+    console.log("building client for browser");
     return axios.create({
-      baseUrl: "/",
+      baseURL: "/",
     });
   }
 };
+
+export default buildClient;
