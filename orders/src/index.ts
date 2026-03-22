bdb9d05f257ff1c0f0e672979ec3ac0f528bca4a
Ankit Singh <ankiitsingh21@gmail.com>
2026-03-22 15:23:51 +0530

    just looging the starting engine in order service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -54,7 +54,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log("Starting")
+    console.log("Starting Enginee ....")
     console.log(`Listening on ${3000}`);
   });
 };
